1eeeb3b4de76b0cbab06963e94be453a50788af6
kerem-kirici <kerem.kirici36@gmail.com>
2025-10-31 19:49:05 +0300

    header aligns2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({
     <LanguageProvider>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <Header />
           <div className="mx-auto max-w-4xl px-8 md:px-6">
-            <Header />
             <main>{children}</main>
             <Footer />
           </div>
