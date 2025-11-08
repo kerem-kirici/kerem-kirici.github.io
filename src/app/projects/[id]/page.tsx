b38b613fd3f73f51c5519689f9d4e857df058745
kerem-kirici <kerem.kirici36@gmail.com>
2025-11-08 17:30:41 +0300

    deploy fix

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -5,6 +5,22 @@ import ProjectDetailLayout from '@/components/projects/ProjectDetailLayout';
 import { getProjectBySlug } from '@/data/projects';
 import { notFound, useParams } from 'next/navigation';
 
+// This function tells Next.js which "id" values to build
+export async function generateStaticParams() {
+  // Let's say you have 3 projects with these IDs:
+  const projects = [
+    { id: 'pokerist' },
+    { id: 'sudokumobileapp' },
+    { id: '2048-playing-algorithm' },
+    { id: 'sudokugenerator' },
+  ];
+
+  // Return an array of objects, where each object has an "id" property
+  return projects.map((project) => ({
+    id: project.id,
+  }));
+}
+
 export default function ProjectDetailPage() {
   const { id } = useParams();
 
